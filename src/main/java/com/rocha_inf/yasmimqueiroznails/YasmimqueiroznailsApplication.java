9d1808c6d142f4bcab792e12c9ca09f9d4c14745
rocha-inf <rochainfnet@gmail.com>
2026-08-29 21:48:22 -0300

    first commit

---
+++ src/main/java/com/rocha_inf/yasmimqueiroznails/YasmimqueiroznailsApplication.java
@@ -0,0 +1,13 @@
+package com.rocha_inf.yasmimqueiroznails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YasmimqueiroznailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YasmimqueiroznailsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rocha_inf/yasmimqueiroznails/YasmimqueiroznailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rocha_inf.yasmimqueiroznails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YasmimqueiroznailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
