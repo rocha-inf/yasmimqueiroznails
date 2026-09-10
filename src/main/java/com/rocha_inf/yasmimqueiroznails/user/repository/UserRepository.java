5883ea731d5a8956587ed55a6d15f051c4e7ce77
rocha-inf <rochainfnet@gmail.com>
2026-09-10 00:12:31 -0300

    feat: adiciona métodos em UserRepository

--- src/main/java/com/rocha_inf/yasmimqueiroznails/user/repository/UserRepository.java
+++ src/main/java/com/rocha_inf/yasmimqueiroznails/user/repository/UserRepository.java
@@ -3,8 +3,9 @@ package com.rocha_inf.yasmimqueiroznails.user.repository;
 import com.rocha_inf.yasmimqueiroznails.user.entity.User;
 import com.rocha_inf.yasmimqueiroznails.user.enums.UserStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
@@ -12,5 +13,9 @@ public interface UserRepository extends JpaRepository<User, UUID> {
     boolean existsByEmailAndStatusNot(String email, UserStatus status);
 
     boolean existsByPhoneNumberAndStatusNot(String phoneNumber, UserStatus status);
+
+    Optional<UserDetails> findUserByEmail(String email);
+
+    Optional<User> findUserByIdAndDeletedAtIsNull(UUID id);
 
 }
