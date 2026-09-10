7c89d54ed7879ca38096b9936445e7d6eb0acc33
rocha-inf <rochainfnet@gmail.com>
2026-09-10 00:16:25 -0300

    feat: adiciona AuthConfig

---
+++ src/main/java/com/rocha_inf/yasmimqueiroznails/security/config/AuthConfig.java
@@ -0,0 +1,23 @@
+package com.rocha_inf.yasmimqueiroznails.security.config;
+
+import com.rocha_inf.yasmimqueiroznails.user.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthConfig implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public AuthConfig(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findUserByEmail(username).orElseThrow(() -> new UsernameNotFoundException(username));
+    }
+
+}
