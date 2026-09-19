450dee29676a5be95e5551223f01938960cd5d0c
rocha-inf <rochainfnet@gmail.com>
2026-09-19 00:07:46 -0300

    feat: adiciona dados dos dias da semana

---
+++ src/main/resources/db/migration/V10__seed_day_of_week.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO day_of_week (value, name)
+    VALUES (1, 'MONDAY'),
+           (2, 'TUESDAY'),
+           (3, 'WEDNESDAY'),
+           (4, 'THURSDAY'),
+           (5, 'FRIDAY'),
+           (6, 'SATURDAY'),
+           (7, 'SUNDAY');
